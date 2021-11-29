b652322d456c7285d41210084a3ed0cc74e8ab28
Yogesh  Sambare <bloggingscan@gmail.com>
2021-11-29 15:31:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const app = express()
-app.use(express.json())
 const connect = () => {
-    return mongoose.connect("mongodb://127.0.0.1:22017\library")
-}
+    return mongoose.connect("mongodb://127.0.0.1:27017\library")
+};
 
 // schema for Naukri candidate 
 
@@ -17,9 +15,63 @@ const userSchema = new mangoose.Schema({
     rating : {type :Number , require:true},
     company : {type :Number , require:true}
 },{
-    versionKey:false
-    timestamps:true
-}) 
+    versionKey:false,
+    timestamps:true,
+}) ;
 
-const User = mongoose.model("user", userSchema)
+const companySchema = new mongoose.Schema(
+    {
+        company_name : {type:String, required: true},
+        openings : {type: Number,required: false},
+        user_id : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref: "user"
+            required: true
+        },
+        job_id : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "job",
+            required : true,
+        },
+    },
+    {
+        versionKey : false,
+        timestamps: true;
+    }
+);
 
+// starting schema for jobs openings
+
+const jobSearch = new mongoose.mongoose.Schema(
+    {
+        job_name : {type: String, required: true},
+        rating : { type: Number, required: false},
+        word_from_home: {type : String, required: true},
+        notice_period : {type : String, required: true},
+        city : {type : String, required: true},
+        skills : {
+            type  :mongoose.Schema.Types.ObjectId,
+            ref: "skills",
+            required: true
+        },
+    },
+    {
+        versionKey : false,
+        timestamps : true,
+    }
+);
+
+const Skills = mangoose.model("skills", skillSchema);
+const app = express();
+app.use(express.json());
+
+// candidate means user crud
+
+app.post("/users", async (req, res) => {
+    try {
+        constt user = await User.create(req.body);
+        return res.status(201).send(user);
+    } catch(e) {
+        return res.status(500).json({message,status: "Failed"});
+    }
+});
